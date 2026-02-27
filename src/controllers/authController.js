a9e601649adf4d8a7b6f0e0154728bd538422e58
Guilherme Sassi <dsrlikeaboss@gmail.com>
2026-02-27 08:31:49 -0300

    feat: Criação do controller e middleware para as rotas solicitdas

---
+++ src/controllers/authController.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { users } = require('../database');
+const expireTime = '1h';
+
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  const user = users.find(u => u.email === email);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciais inválidas' });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    return res.status(401).json({ error: 'Credenciais inválidas' });
+  }
+
+  const token = jwt.sign(
+    { id: user.id, email: user.email, type: user.type },
+    process.env.JWT_SECRET,
+    { expiresIn: expireTime }
+  );
+
+  return res.json({ token });
+}
+
+module.exports = { login };
